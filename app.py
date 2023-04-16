c056306411153a74e1184751e8397b6f411a5e5c
Sruthij93 <sjayachandran@nevada.unr.edu>
2023-04-16 00:41:25 -0700

    mainpage

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 @app.route('/')
+def main():
+    return render_template('mainpage.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
